bd2040ff93dc59e9f7c94a621f830e52d14f5cca
RossanaPalma <70126235+RossanaPalma@users.noreply.github.com>
2021-10-25 23:47:48 -0700

    Add project files.

---
+++ Homework_week9_Project1/Odometer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework_week9_Project1
+{
+    class Odometer
+    {
+        private int miles;
+        private double fuelUsed;
+        //Constructors
+        public Odometer()
+        {
+
+        }
+        public Odometer(int miles, double fuelUsed)
+        {
+            this.miles = miles;
+            this.fuelUsed = fuelUsed;
+        }
+        //Quick actions
+        public int Miles { get => miles; set => miles = value; }
+
+        public double FuelUssed { get => fuelUsed; set => fuelUsed = value; }
+
+        //Methods
+        public void reset()
+        {
+            miles = 0;
+            fuelUsed = 0;
+        }
+        public void add(int m, double f)
+        {
+            miles += m; // miles = miles + m;
+            fuelUsed += f; // fuelUsed = fuelUsed + f;
+        }
+        public double MPG()
+        {
+            return (miles / fuelUsed);
+        }
+        public String toString()
+        { 
+            return " " + miles + " miles driven " + fuelUsed + " fuel used";
+        }
+    }
+}
---
+++ Homework_week9_Project1/Program.cs
@@ -0,0 +1,51 @@
+﻿/// Chapter No. 11 - Homework9_Project1
+/// File Name: Homework9_week9_Project1.java
+/// @author: Rossana Palma
+/// Date:  October 18, 2021
+///
+/// Problem Statement:  Define a class called Odometer that will be used to track 
+/// fuel usage and miles per gallon.    
+/// 
+/// Overall Plan:
+/// 1) Create an Odometer class
+/// 2) Create two constructors, 1 constructor should be the no-argument constructor 
+/// the other will set the two instance variables.
+/// 3) Include a reset method that sets the mileage drive and the fuel used to zero
+/// 4) Include an add method which takes two parameters that add to the fuel consumed 
+/// and the miles driven.
+/// 5) create a method that returns the miles per gallon 
+/// and an appropriate ToString method
+/// 6) Write a driver program that tests all the methods and constructors.  
+
+
+using System;
+
+namespace Homework_week9_Project1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Odometer trip1 = new Odometer(); // 1st Odometer : class name  2nd Odometer : constructor
+            trip1.add(50, 10);
+            Console.WriteLine("Fuel used for trip 1 is: " + trip1.MPG());
+            Odometer trip2 = new Odometer();
+            trip2.add(100, 20);
+            Console.WriteLine("Fuel used for trip 2 is: " + trip2.MPG());
+            trip1.reset();
+            Console.WriteLine("Fuel used for trip 1 is: " + trip1.toString());
+
+            trip2.add(150, 0);
+            Console.WriteLine("Fuel used for trip 2 is: " + trip2.MPG());
+
+
+            Console.ReadLine();
+                                      
+        }
+        public static int Addition(int n1, int n2)
+        {
+            return n1 + n2;
+        }
+
+    }
+}
